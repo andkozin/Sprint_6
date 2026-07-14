03ba18096ba0dfe504fd0bde90e51eb379abcf53
Andrey Kozin <andkozin@gmail.com>
2026-07-14 14:08:32 +0300

    Заполнил tests/data.py

---
+++ tests/data.py
@@ -0,0 +1,28 @@
+# tests/data.py
+
+
+ORDER_DATA_SET_1 = {
+    "name": "Петр",
+    "surname": "Петров",
+    "address": "СПб, пгт. Рощино",
+    "metro": "ВДНХ",
+    "phone": "+79990000001",
+    "duration": "сутки",
+    "button_location": "top",
+    "order_date" : "3" ,
+    "scooter_color":"black",
+    "comments" : " Домофон - 12345"
+}
+
+ORDER_DATA_SET_2 = {
+    "name": "Иван",
+    "surname": "Иванов",
+    "address": "Мск. пр. Ленина, д. 1",
+    "metro": "Охотный Ряд",
+    "phone": "+79990000002",
+    "duration": "трое суток",
+    "button_location": "finish", 
+    "order_date" : "8" , 
+    "scooter_color":"grey",
+    "comments" : " Домофон - 54321"
+}
