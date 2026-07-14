c85dc4cce43a104ed58b617b18a5dac402d8f5f4
Andrey Kozin <andkozin@gmail.com>
2026-07-14 14:03:09 +0300

    Заполнил  pages/locators.py

---
+++ pages/locators.py
@@ -0,0 +1,54 @@
+# pages/locators.py
+from selenium.webdriver.common.by import By
+
+# локаторы для главной страницы
+class MainPageLocators: 
+   
+    ACCORDION_HEADERS = (By.CSS_SELECTOR, "[data-accordion-component='AccordionItemButton']") # для списка заголовок
+    PANEL_INDEX = "accordion__panel-{}" # для индекст заголовка
+    ORDER_BUTTON_TOP = (By.XPATH, "//div[contains(@class,'Header_Nav')]//button[.='Заказать']") # кнопка заказать в шапке
+    ORDER_FINISH_BUTTON = (By.XPATH, "//div[contains(@class,'Home_FinishButton')]//button[.='Заказать']") # кнопка заказать внизу
+
+class OrderFormLocators:
+
+    CONFIRM_BUTTON = (By.ID, "rcc-confirm-button") # для баннера cookie
+
+    FORM_HEADER_1 = (By.XPATH, "//div[contains(text(), 'Для кого самокат')]") #для формы заполнения 1 (имя) Для кого самокат
+
+    # Поля первого шага
+    INPUT_NAME = (By.XPATH, "//input[contains(@placeholder, 'Имя')]") # поле Имя
+    INPUT_SURNAME = (By.XPATH, "//input[contains(@placeholder, 'Фамилия')]") # поле Фамилия
+    INPUT_ADDRESS = (By.XPATH, "//input[contains(@placeholder, 'Адрес: куда привезти заказ')]") # поле Адрес
+    INPUT_PHONE = (By.XPATH, "//input[contains(@placeholder, 'Телефон: на него позвонит курьер')]") # поле Телефон
+
+    # Метро
+    INPUT_METRO_SEARCH = (By.CSS_SELECTOR, "input.select-search__input") # поле Метро
+    METRO_OPTION = (By.CSS_SELECTOR, ".select-search__option") #  для опции метро
+    
+    BUTTON_NEXT = (By.XPATH, "//button[normalize-space()='Далее']") # копка Далее в первой форме ввода
+    
+    # Дата и календарь
+    DATE_INPUT = (By.XPATH, "//input[@placeholder='* Когда привезти самокат']") # поле выбора дата
+    CALENDAR_DAY = (By.CSS_SELECTOR, ".react-datepicker__day:not(.react-datepicker__day--outside-month)") # раскрыть календарь
+
+    #  срока аренды
+    DURATION_TRIGGER = (By.CSS_SELECTOR, ".Dropdown-control[aria-haspopup='listbox']") # Триггер (область, по которой кликаем, чтобы открыть список)
+    DURATION_MENU    = (By.CSS_SELECTOR, ".Dropdown-menu[aria-expanded='true']") # Меню (список опций)
+    DURATION_OPTION  = (By.CSS_SELECTOR, ".Dropdown-option[role='option']") # Опции внутри меню
+
+    DURATION_DROPDOWN = (By.CSS_SELECTOR, "div.Dropdown-root div.Dropdown-control[aria-haspopup='listbox']") # срок аренды про поле для проверок
+
+    # Чекбоксы цвета
+    CHECKBOX_BLACK = (By.ID, "black") # чек бокс черный
+    CHECKBOX_GREY = (By.ID, "grey") # чек бокс серый
+
+    COMMENT_FIELD = (By.CSS_SELECTOR, 'input[placeholder="Комментарий для курьера"]') # поле комментарий
+
+    ORDER_BUTTON_IN_FORM_2 = (By.XPATH, '//div[contains(@class, "Order_Buttons")]//button[normalize-space()="Заказать"]') # кнопка заказать 2 формы ввода
+    CONFIRM_BUTTON_YES = (By.XPATH, '//div[contains(@class, "Order_Modal")]//button[normalize-space()="Да"]') # кнопка ДА подтверждения заказа
+    SUCCESS_MODAL_HEADER = (By.XPATH, '//div[contains(@class, "Order_Modal")]//div[contains(@class, "Order_ModalHeader")]') # окно создания заказа
+    VIEW_STATUS_BUTTON = (By.XPATH, '//div[contains(@class, "Order_NextButton")]//button[normalize-space()="Посмотреть статус"]') # кнопка для статуса заказа
+
+    LOGO_LINK_SAMOKAT = (By.CSS_SELECTOR, 'a[class*="Header_LogoScooter"]') # лого Самоката на ГС
+    LOGO_YANDEX_LINK = (By.CSS_SELECTOR, 'a[class*="Header_LogoYandex"]') # лого Яндекса
+    
