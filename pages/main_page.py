ff5db96960ce1b558098258100ce2f2873ba8fa8
Andrey Kozin <andkozin@gmail.com>
2026-07-22 14:52:11 +0300

    Добавил @allure.step в main_page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,5 @@
 # pages/main_page.py
+import allure
 from selenium.webdriver.common.by import By
 from .base_page import BasePage
 from locators.main_page_locators import MainPageLocators
@@ -6,12 +7,15 @@ from locators.main_page_locators import MainPageLocators
 class MainPage(BasePage):
 
     # собрал по индексу
+    
     @staticmethod
+    @allure.step("Получаем локатор панели по индексу {index}")
     def _get_panel_locator(index: int):
         panel_id = MainPageLocators.PANEL_INDEX.format(index)
         return By.ID, panel_id
 
     # "top" или "bottom"
+    @allure.step("Кликаем кнопку заказа: расположение = {location}")
     def click_order_button(self, location: str):
             
             if location == "top":
@@ -21,6 +25,7 @@ class MainPage(BasePage):
             
             self.click_with_fallback(locator)
 
+    @allure.step("Кликаем заголовок аккордеона по индексу: {index}")
     def click_accordion_header_by_index(self, index: int)-> None:
         self.close_cookie_banner()
 
@@ -34,6 +39,7 @@ class MainPage(BasePage):
         self.scroll_into_view(header)
         self.click_with_fallback(header)
 
+    @allure.step("Ждём и получаем текст панели по индексу: {index}")
     def wait_for_text_by_index(self, index: int): #str
         panel_loc = self._get_panel_locator(index) # взял по индексу
         panel = self.wait_for_element_visible(panel_loc) # ghjdthbk
